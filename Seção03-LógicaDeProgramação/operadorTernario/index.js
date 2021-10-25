d900eee5735e238f9de1a8ba003fa2db26d85263
NcryScooby <fabricio_c_anelli@hotmail.com>
2021-10-25 11:10:38 -0300

    operador ternario

--- Seção03-LógicaDeProgramação/operadorTernario/index.js
+++ Seção03-LógicaDeProgramação/operadorTernario/index.js
@@ -1,3 +1,31 @@
-// Operador Ternário ? :
+// Operador Ternário ? : - se ? senão :
 
-//teste
+// ? Verdadeiro   
+// : Falso
+
+// Declara a pontuação do usuário
+const pontos = 1000;
+
+// Se pontos for maior ou igual a 1000, é usuário VIP senão é usuário normal
+const statusUsuario = pontos >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+
+// if (pontos >= 1000) {
+//   const pontosUsuario = 'Usuário VIP';
+//   console.log(pontosUsuario);
+//} else {
+//    const pontosUsuario = 'Usuário normal';
+//    console.log(pontosUsuario);
+//}
+
+// Exibe se é usuário VIP ou normal
+console.log(statusUsuario);
+
+
+// Diz que corUsuario é nulo
+const corUsusario = 'pink';
+
+// Se corUsuario for nulo, então corUsuario é preto, chamamos isso de fallback
+const corPadrao = corUsusario || 'Preto';
+
+// Exibite a cor do Padrão
+console.log(corPadrao);
